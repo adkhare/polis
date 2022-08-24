a8ac83370d8eb7a432c705a77dd4155178703f6e
amit-khare <amit@mailchimp.com>
2022-08-23 21:07:15 -0400

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export PATH=$PATH:/usr/local/go/bin
+echo "export GOROOT=/usr/local/go" >> ~/.profile
+echo "export GOPATH=\$HOME/go" >> ~/.profile
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
